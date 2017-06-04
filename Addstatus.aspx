cfd97d6aa4f93309e4e538e480f702f77ef58023
surajpatil311 <mail2surajkumar@gmail.com>
2017-06-03 21:20:41 -0700

    Add files via upload

---
+++ Addstatus.aspx
@@ -0,0 +1,167 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/master.Master" CodeBehind="Addstatus.aspx.cs" Inherits="ThreeWebApplication2.Addstatus" %>
+
+<asp:Content ID="content" runat="server" ContentPlaceHolderID="head">
+    
+    <script src="js/tcal.js"></script>
+    <link href="CSS/tcal.css" rel="stylesheet" />
+
+</asp:Content>
+
+<asp:Content ID="content1" runat="server" ContentPlaceHolderID="ContentPlaceHolder1">
+
+    <div style="width:100%">
+        <table style="margin-left:45%">
+            <tr>
+                <td style="text-align:left">
+                <asp:Label ID="Label1" runat="server" Text="Add Status" Font-Bold="True" Font-Overline="False" Font-Size="Larger" Enabled="False" EnableViewState="False"></asp:Label>
+                </td>
+                </tr>
+         </table>
+        <div>
+             <div style="margin-left:5%"> 
+        <table> 
+            <tr>
+                <td>
+                    <asp:Label ID="Label2" runat="server" Text="Search By Name" Font-Bold="True"></asp:Label>
+                </td>
+                <td>
+                    <asp:DropDownList CssClass="form-control input-md" ID="DropDownList1" runat="server" AutoPostBack="true" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                        <asp:ListItem>Select</asp:ListItem>
+                        <asp:ListItem>Client Name</asp:ListItem>
+                        <asp:ListItem>Task Name</asp:ListItem>
+                        <asp:ListItem>Sub Task Name</asp:ListItem>
+                        <asp:ListItem>Employee Name</asp:ListItem>
+                        <asp:ListItem>Date</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+                <td colspan="2">
+                    <asp:TextBox CssClass="form-control input-md" ID="txtsearch" runat="server"></asp:TextBox>
+
+                </td>
+                <td>
+                    <asp:Button CssClass="btn btn-info"  ID="Button3" runat="server" Text="Search" OnClick="search_Click" />
+                </td>
+            </tr>
+           </table>
+          </div>
+
+          <div style="margin-left:5%" id="from1" runat="server">
+           <table>
+                <tr>
+                <td> <asp:Label ID="Label3" runat="server" Text="From" Font-Bold="True"></asp:Label>
+
+                </td>
+                <td style="margin-left:25%">
+                    <asp:TextBox class="tcal" ID="From" runat="server" Width="80px" Height="20px"></asp:TextBox></td>
+                <td>
+                    <asp:Label ID="Label4" runat="server" Text="To" Font-Bold="True"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox class="tcal" ID="To" runat="server" Width="80px" Height="20px"></asp:TextBox>
+                </td>
+            </tr>
+               </table>
+            </div>
+   
+        <div style="margin-left:5%">
+
+            <asp:GridView CssClass="table table-condensed table-hover table-striped" ID="GridView1" runat="server" AutoGenerateColumns="False" GridLines="None" AllowPaging="True" OnPageIndexChanging="GridView1_PageIndexChanging" OnDataBound="GridView1_DataBound" PageSize="7" >
+                <Columns>
+                    <asp:TemplateField HeaderText="SrNo" SortExpression="srno">
+                        
+                        <ItemTemplate>
+                            <asp:Label ID="lablesrno" runat="server" Text='<%# Eval("srno") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="ClientName" SortExpression="clientname">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TextBox1" runat="server" Text='<%# Bind("clientname") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="lableclientname" runat="server" Text='<%# Bind("clientname") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="TaskName" SortExpression="taskname">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TextBox2" runat="server" Text='<%# Bind("taskname") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="labletaskname" runat="server" Text='<%# Bind("taskname") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="SubtaskName" SortExpression="subtaskname">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TextBox3" runat="server" Text='<%# Bind("subtaskname") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="lablesubtaskname" runat="server" Text='<%# Bind("subtaskname") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Employee Name" SortExpression="empname">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TextBox4" runat="server" Text='<%# Bind("empname") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="lableempname" runat="server" Text='<%# Bind("empname") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Assigndate" SortExpression="assigndate">
+                        <EditItemTemplate>
+                            <asp:TextBox ID="TextBox5" runat="server" Text='<%# Bind("assigndate") %>'></asp:TextBox>
+                        </EditItemTemplate>
+                        <ItemTemplate>
+                            <asp:Label ID="lableassigndate" runat="server" Text='<%# Bind("assigndate", "{0:dd-M-yyyy}") %>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="DeadlineDate">
+                        <ItemTemplate>
+                               <asp:TextBox ID="txtdeadlinedate" runat="server" Width="111px" CssClass="tcal" Text='<%#Eval("deadlinedate") %>'>'></asp:TextBox>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                   <%-- <asp:BoundField DataField="Deadline Date" HeaderText="DeadlineDate" SortExpression="deadlinedate" />--%>
+                    <asp:TemplateField HeaderText="Remark">
+                        <ItemTemplate>
+                            <asp:TextBox CssClass="form-control input-md" ID="Remark" runat="server" Text='<%#Eval("remark") %>'>'></asp:TextBox>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                   <%-- <asp:BoundField DataField="Remark" HeaderText="Remark" SortExpression="remark" />--%>
+                    <asp:TemplateField HeaderText=" Status">
+                    <ItemTemplate>
+
+                        <asp:DropDownList ID="DropDownList1" runat="server" DataTextField="status" DataValueField="status" AutoPostBack="true" AppendDataBoundItems="true"  OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged1">
+                            <asp:ListItem>Select</asp:ListItem>
+                        <asp:ListItem>Inprogress</asp:ListItem>
+                        <asp:ListItem>Pending</asp:ListItem>
+                        <asp:ListItem>Complete</asp:ListItem>
+                            </asp:DropDownList>
+                   
+                    </ItemTemplate>
+                        
+                </asp:TemplateField>
+
+
+                </Columns>
+            </asp:GridView>
+        </div>   <div style="margin-left:45%">
+        
+    </div>
+        </div>
+    <div style="width:100%">
+        <table style="margin-left:45%">
+            <tr>
+                <td>
+                    <asp:Button CssClass="btn btn-success" ID="save" runat="server" Text="Save" OnClick="save_Click1"/>
+                </td>
+                <td>
+                    <asp:Button CssClass="btn btn-warning" ID="Button2" runat="server" Text="Cancel"/>
+                    
+                </td>
+                
+            </tr>
+            
+            
+        </table>
+    </div>
+        
+</asp:Content>
+
---
+++ Addstatus.aspx.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using synbuf.component.bl;
+using System.Configuration;
+using System.Data;
+
+namespace ThreeWebApplication2
+{
+    public partial class Addstatus : System.Web.UI.Page
+    {
+        AssignSubTaskDetails cdl = new AssignSubTaskDetails();
+        AssignSubTaskBL cbl = new AssignSubTaskBL();
+       
+        public Addstatus()
+        {
+            connectionDB();
+        }
+        public bool connectionDB()
+        {
+            return cbl.connection(ConfigurationManager.AppSettings["connectionstring"]);
+        }
+        protected void Page_Load(object sender, EventArgs e)
+       {
+           if (!IsPostBack)
+           {
+               from1.Visible = false;
+               DataTable dt = new DataTable();
+               dt = cbl.selecttable();
+               GridView1.DataSource = dt;
+               GridView1.DataBind();
+               SetInitialRow();
+           }
+        }
+        private void SetInitialRow()
+        {
+
+            DataTable dt = new DataTable();
+            DataRow dr = null;
+            dt.Columns.Add(new DataColumn("Srno", typeof(string)));
+            dt.Columns.Add(new DataColumn("clientname", typeof(string)));
+            dt.Columns.Add(new DataColumn("taskname", typeof(string)));
+            dt.Columns.Add(new DataColumn("subtaskname", typeof(string)));
+            dt.Columns.Add(new DataColumn("empname", typeof(string)));
+            dt.Columns.Add(new DataColumn("assigndate", typeof(string)));
+            dt.Columns.Add(new DataColumn("DeadlineDate", typeof(string)));
+            dt.Columns.Add(new DataColumn("Remark", typeof(string)));
+            dt.Columns.Add(new DataColumn("Status", typeof(string)));
+            DataTable data1 = new DataTable();
+            data1=cbl.getdataingridview();
+            for(int i=0;i<data1.Rows.Count;i++)
+            {
+                if (data1.Rows[i].ItemArray[5].ToString() == "")
+                {
+                    dr = dt.NewRow();
+                    dr["Srno"] = data1.Rows[i].ItemArray[0].ToString();
+                    dr["clientname"] = data1.Rows[i].ItemArray[1].ToString();
+                    dr["taskname"] = data1.Rows[i].ItemArray[2].ToString();
+                    dr["subtaskname"] = data1.Rows[i].ItemArray[3].ToString();
+                    dr["empname"] = data1.Rows[i].ItemArray[4].ToString();
+                    dr["assigndate"] = data1.Rows[i].ItemArray[6].ToString();
+                    dr["DeadlineDate"] = data1.Rows[i].ItemArray[7].ToString();
+                    dr["Remark"] = data1.Rows[i].ItemArray[9].ToString();
+                    dr["Status"] = string.Empty;
+                }
+                else
+                {
+                    dr = dt.NewRow();
+                    dr["Srno"] = data1.Rows[i].ItemArray[0].ToString();
+                    dr["clientname"] = data1.Rows[i].ItemArray[1].ToString();
+                    dr["taskname"] = data1.Rows[i].ItemArray[2].ToString();
+                    dr["subtaskname"] = data1.Rows[i].ItemArray[3].ToString();
+                    dr["empname"] = data1.Rows[i].ItemArray[5].ToString();
+                    dr["assigndate"] = data1.Rows[i].ItemArray[6].ToString();
+                    dr["DeadlineDate"] = data1.Rows[i].ItemArray[7].ToString();
+                    dr["Remark"] = data1.Rows[i].ItemArray[9].ToString();
+                    dr["Status"] = string.Empty;
+                }
+                dt.Rows.Add(dr);
+
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+       
+        }
+        
+
+        protected void save_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void save_Click1(object sender, EventArgs e)
+        {
+            int rowIndex = 0;
+            for (int i = 1; i <= GridView1.Rows.Count; i++)
+            {
+                Label lblsrno = (Label)GridView1.Rows[rowIndex].Cells[0].FindControl("lablesrno");
+                cdl.srno = Convert.ToInt32(lblsrno.Text);
+                Label lblclientname = (Label)GridView1.Rows[rowIndex].Cells[1].FindControl("lableclientname");
+               cdl.clientname = lblclientname.Text;
+               Label lbltaskname = (Label)GridView1.Rows[rowIndex].Cells[2].FindControl("labletaskname");
+               cdl.taskname = lbltaskname.Text;
+               Label lblsubtaskname = (Label)GridView1.Rows[rowIndex].Cells[3].FindControl("lablesubtaskname");
+               cdl.subtaskname = lblsubtaskname.Text;
+               Label lblempname = (Label)GridView1.Rows[rowIndex].Cells[4].FindControl("lableempname");
+               cdl.empname = lblempname.Text;
+               Label lblassigndate = (Label)GridView1.Rows[rowIndex].Cells[5].FindControl("lableassigndate");
+               cdl.assigndate = lblassigndate.Text;
+               TextBox box2 = (TextBox)GridView1.Rows[rowIndex].Cells[6].FindControl("labletxtdeadlinedate");
+               
+                TextBox box1 = (TextBox)GridView1.Rows[rowIndex].Cells[7].FindControl("Remark");
+
+                DropDownList dp1 = (DropDownList)GridView1.Rows[rowIndex].Cells[8].FindControl("DropDownList1");
+                cdl.status = dp1.SelectedItem.Text;
+                cdl.remark = box1.Text;
+
+                //cdl.deadlinesdate = box2.Text;
+                cdl.approvetask = "false";
+                cbl.updateaddstatus(cdl);
+
+                rowIndex++;
+
+            }
+            string message = "Add Status Succesfully";
+            string url = "Home1.aspx";
+            string script = "window.onload = function(){ alert('";
+            script += message;
+            script += "');";
+            script += "window.location = '";
+            script += url;
+            script += "'; }";
+            ClientScript.RegisterStartupScript(this.GetType(), "Redirect", script, true);
+        }
+
+        protected void search_Click(object sender, EventArgs e)
+        {
+             DataTable dt = new DataTable();
+           
+            if (DropDownList1.SelectedItem.Text == "Client Name")
+            {
+                cdl.search = txtsearch.Text;
+                dt = cbl.searchrecord(cdl);
+            }
+            else if (DropDownList1.SelectedItem.Text == "Task Name")
+            {
+                cdl.search = txtsearch.Text;
+                dt = cbl.searchbytaskname(cdl);
+            }
+            else if (DropDownList1.SelectedItem.Text == "Sub Task Name")
+            {
+                cdl.search = txtsearch.Text;
+                dt = cbl.searchbysubtaskname(cdl);
+            }
+            else if (DropDownList1.SelectedItem.Text == "Employee Name")
+            {
+                cdl.search = txtsearch.Text;
+                dt = cbl.searchbyempname(cdl);
+            }
+            else if (DropDownList1.SelectedItem.Text == "Date")
+            {
+                cdl.search = txtsearch.Text;
+                dt = cbl.searchbyassigndate(cdl);
+            }
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             if (DropDownList1.SelectedItem.Text == "Date")
+            {
+                from1.Visible = true;
+                
+            }        
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt = cbl.selecttable();
+            GridView1.DataSource = dt;
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataBind();
+            
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedItem.Text == "Date")
+            {
+                from1.Visible = true;
+
+            }    
+        }
+
+        protected void GridView1_DataBound(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            dt = cbl.selectstatus();
+            int rowindex = 0;
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < GridView1.Rows.Count; i++)
+                {
+                    DropDownList box1 = (DropDownList)GridView1.Rows[rowindex].Cells[8].FindControl("DropDownList1");
+                    box1.SelectedItem.Text = dt.Rows[i].ItemArray[10].ToString();
+                 
+                    rowindex++;
+                }
+            }
+        
+        }
+
+        protected void DropDownList2_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+}
+}
+        
+
+
